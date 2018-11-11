353831c882dd357a642a77eef2171a16371e1532
Sammeln <kirilln272@gmail.com>
2018-11-11 16:19:04 +0300

    Доделал ДЗ

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,32 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebStore.Models;
 
-namespace WebStore.controllers
+namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<EmployeeViewModel> employees = new List<EmployeeViewModel>
-            {
-                new EmployeeViewModel(1,"Иван","Иванов","Иванович","Продажи",new DateTime(1989,3,25),new DateTime(2012,9,10)),
-                new EmployeeViewModel(2,"Сергей","Петров","Дмитриевич","Производство",new DateTime(1975,5,2),new DateTime(2011,2,1)),
-                new EmployeeViewModel(3,"Владимир","Родионов","Петрович","Бухгалтерия",new DateTime(1983,10,17),new DateTime(2008,5,30)),
-                new EmployeeViewModel(4,"Дмитрий","Трофимов","Михайлович","Снабжение",new DateTime(1992,4,23),new DateTime(2005,11,26)),
-            };
         public IActionResult Index()
         {
-            return View(employees);
+            return View();
         }
-
-        public IActionResult Details(int id)
+        public IActionResult Cart()
         {
-            EmployeeViewModel employee = employees.FirstOrDefault(e => e.Id == id);
-            if (ReferenceEquals(employee, null))
-            {
-                return NotFound();
-            }
-            return View(employee);
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+        public IActionResult _404()
+        {
+            return View();
         }
     }
 }
